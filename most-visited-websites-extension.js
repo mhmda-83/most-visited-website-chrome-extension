e52fccc044be6e4914c3617c5c5d52d263987bd9
Mohammad MohammadAlian <mralian.dev@gmail.com>
2020-03-12 13:28:46 +0330

    remove www and http from urls

--- most-visited-websites-extension.js
+++ most-visited-websites-extension.js
@@ -1,26 +1,44 @@
+const getDomain = (url, subdomain) => {
+  subdomain = subdomain || false;
+
+  url = url.replace(/(https?:\/\/)?(www.)?/i, '');
+
+  if (!subdomain) {
+      url = url.split('.');
+
+      url = url.slice(url.length - 2).join('.');
+  }
+
+  if (url.indexOf('/') !== -1) {
+      return url.split('/')[0];
+  }
+
+  return url;
+};
+
 chrome.storage.sync.get('mostVisitedWebsites', value => {
   // check if no value set yet
   if (Object.keys(value).length === 0) {
     return chrome.storage.sync.set({
       mostVisitedWebsites: [
-        { url: window.location.hostname, numberOfVisits: 1 }
+        { url: getDomain(window.location.hostname, true), numberOfVisits: 1 }
       ]
     });
   }
   // check if url exists
   const isOldUrl = value.mostVisitedWebsites.find(el => {
-    return el.url === window.location.hostname;
+    return el.url === getDomain(window.location.hostname, true);
   });
   if (!isOldUrl) {
     // if new url add to list
     value.mostVisitedWebsites.push({
-      url: window.location.hostname,
+      url: getDomain(window.location.hostname, true),
       numberOfVisits: 1
     });
   } else {
     // if it's not a new url add numberOfVisits
     let newValue = value.mostVisitedWebsites.map(value => {
-      if (value.url === window.location.hostname) {
+      if (value.url === getDomain(window.location.hostname, true)) {
         value.numberOfVisits += 1;
       }
       return value;
